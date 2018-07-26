262791105065b477ab117c2117ebbcdaf34ffa1c
Eran Otzap <eranotz50@gmail.com>
2018-07-27 01:21:39 +0300

    defaults before media queries

--- public/js/brokersViewCntrl.js
+++ public/js/brokersViewCntrl.js
@@ -1,12 +1,12 @@
 function brokersViewCntrl(container){
 
-  var brokerTemplate = "<li class='broker'>"
-  + "<div class='inline-header col1'>Broker</div> <div  class='inline-header col2'>Broker</br>Rating</div><div class='inline-header col3'>Maximum </br> Leverage </div>"
+  var brokerTemplate = "<div class='broker'>"
+  + "<div class='inline-header col1'></div> <div  class='inline-header col2'>Broker</br>Rating</div><div class='inline-header col3'>Maximum </br> Leverage </div>"
   + "<div class='inline-header col4'>Regulation  </br> Authority</div><div class='inline-header col5'>Minimum </br> Deposit</div><div class='inline-header col6'>Minimum </br> Spread </div> <div class='inline-header col7'></div>" 
   + "<div class='col1'><img src='{Img}'/></div><div class='col2'>{Rating}</div><div class='col3'>{Leverage}</div>"
   + "<div class='col4'>{Regulatory}</div><div class='col5'>{MinDeposit}</div><div class='col6'>{Spreads}</div>"
   + "<div class='col7'><a target='_blank' href='https://www.fortrade.com?B=567&A=147174&mtId=567'> Open account </a></div>"
-  +"</li>"    
+  +"</div>"    
   
 
   return {
